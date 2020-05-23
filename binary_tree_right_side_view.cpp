da2d4639196da3ac3cb6df0f5bb0d0bbda44149e
Yupeng Han <hanyupeng9406@gmail.com>
2020-05-23 17:19:19 -0400

    add 199

--- binary_tree_right_side_view.cpp
+++ binary_tree_right_side_view.cpp
@@ -1,3 +1,19 @@
+
+// 根据地址的增长顺序，强行用数学解 --> 这是错误解法
+
+// Q how to loop over binary tree (without parent)
+// A Using stack
+/*
+    1 create empty stack
+    2 initialize current node as root
+    3 push currrent node to S and set cur = cur->left until cur->left == nullptr
+    4 if cur->left == nullptr
+        a) pop top of stack
+        b) set cur = poped_item->rihgt
+        c) go to 3
+    5 if cur == nullptr && stack is empty -> DONE
+*/
+
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -12,34 +28,75 @@
 class Solution {
 public:
     vector<int> rightSideView(TreeNode* root) {
-        vector<int> ret;
-        if (root == nullptr) return ret;
+        vector<int> ret_vector;
+        if (root == nullptr) {
+            return ret_vector;
+        }
+        // if (root->left == nullptr && root->right == nullptr) {
+        //     ret_vector.insert(ret_vector.begin(),root->val);
+        //     return ret_vector;
+        // }
         
-        TreeNode *cur_node = root;
-        ret.insert(ret.end(), cur_node->val);
-        cout << cur_node->val << endl;
         
 
+        int max_layer = -1;
+        int cur_layer = 0;
+        stack<pair<int, TreeNode*>> bt; //left, right
+        // bt.push(make_pair(-1, root));
+        // TreeNode *cur_node = root;
 
-        while (haschild(cur_node)) {
-            if (cur_node->right != nullptr) {
-                cur_node = cur_node->right;
-                ret.insert(ret.end(), cur_node->val);
-                cout << cur_node->val << endl;
+        
+        while (root->left != nullptr || root->right != nullptr || root != nullptr || bt.size() > 0) {
+            if (root->right != nullptr) {
+                // cout << root->val << " " << cur_layer << " " << max_layer << " " << bt.size() << endl;
+                if (root->left != nullptr) {
+                    bt.push(make_pair(cur_layer, root));
+                }
+                if (cur_layer > max_layer) {
+                    cout << "add!!" << endl;
+                    max_layer++;
+                    ret_vector.insert(ret_vector.end(), root->val);
+                }
+                root = root->right;
+                cur_layer++;
+                continue;
+            }
+            
+            if (root->left != nullptr) { // right is empty
+                // cout << root->val << " " << cur_layer << " " << max_layer << " " << bt.size() << endl;
+                if (cur_layer > max_layer) {
+                    max_layer++;
+                    cout << "add!!" << endl;
+                    ret_vector.insert(ret_vector.end(), root->val);
+                }
+                root = root->left;
+                cur_layer++;
+                continue;
+            }
+
+            // cout << root->val << " " << cur_layer << " " << max_layer << " " << bt.size() << endl;
+            
+            if (cur_layer > max_layer) {
+                max_layer++;
+                cout << "add!!" << endl;
+                ret_vector.insert(ret_vector.end(), root->val);
+            }
+            
+            if (bt.size() > 0) {
+                pair <int, TreeNode*> temp;
+                temp = bt.top();
+                bt.pop();
+                cur_layer = temp.first+1;
+                root = temp.second->left;
             }
             else {
-                cur_node = cur_node->left;
-                ret.insert(ret.end(), cur_node->val);
-                cout << cur_node->val << endl;
+                break;
             }
+            
+            
         }
-        return ret;
+        return ret_vector;
     }
-    bool haschild(TreeNode * node){
-        if (node->left == nullptr && node->right == nullptr) {
-            return false;
-        }
-        else return true;
-    }
+    
+    
 };
-// 根据地址的增长顺序，强行用数学解

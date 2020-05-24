0c331fafb3a8407733e8e9a67df9803329536b33
Yupeng Han <hanyupeng9406@gmail.com>
2020-05-23 23:46:58 -0400

    add 1192

--- critical_connections_in_a_network.cpp
+++ critical_connections_in_a_network.cpp
@@ -1,57 +1,57 @@
-class Solution {
-public:
+// class Solution {
+// public:
     
-    /*
-        visit all child not including parent, and return the min depth of the child
-    */
-    void dfs(   int current_node,
-                int parent_node,
-                int depth,
-                vector<vector<int>>& adj_list,
-                vector<vector<int>>& bridges,
-                vector<int>& idx_map,
-                vector<int>& jump_map,
-                vector<bool>& visited) {
+//     /*
+//         visit all child not including parent, and return the min depth of the child
+//     */
+//     void dfs(   int current_node,
+//                 int parent_node,
+//                 int depth,
+//                 vector<vector<int>>& adj_list,
+//                 vector<vector<int>>& bridges,
+//                 vector<int>& idx_map,
+//                 vector<int>& jump_map,
+//                 vector<bool>& visited) {
 
-        visited[current_node] = true;
-        idx_map[current_node] = jump_map[current_node] = ++depth;
+//         visited[current_node] = true;
+//         idx_map[current_node] = jump_map[current_node] = ++depth;
 
-        for (auto child : adj_list[current_node]) {
-            if (child == parent_node) continue;
-            else if (visited[child] == false) {
-                dfs(child, current_node, depth, adj_list, bridges, idx_map, jump_map, visited);
-                jump_map[current_node] = min(jump_map[current_node], jump_map[child]);
-                if (idx_map[current_node] < jump_map[child]) {
-                    bridges.push_back({current_node, child});
-                }
-            }
-            else { //visited[child] == true
-                jump_map[current_node] = min(jump_map[current_node], idx_map[child]);
-            }
-        }
+//         for (auto child : adj_list[current_node]) {
+//             if (child == parent_node) continue;
+//             else if (visited[child] == false) {
+//                 dfs(child, current_node, depth, adj_list, bridges, idx_map, jump_map, visited);
+//                 jump_map[current_node] = min(jump_map[current_node], jump_map[child]);
+//                 if (idx_map[current_node] < jump_map[child]) {
+//                     bridges.push_back({current_node, child});
+//                 }
+//             }
+//             else { //visited[child] == true
+//                 jump_map[current_node] = min(jump_map[current_node], idx_map[child]);
+//             }
+//         }
 
-    }
+//     }
 
-    vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connections) {
-        // transform into adjacent_list
-        vector<vector<int>> adj_list(n);
-        for(auto con : connections) {
-            adj_list[con[0]].push_back(con[1]);
-            adj_list[con[1]].push_back(con[0]);
-        }
+//     vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connections) {
+//         // transform into adjacent_list
+//         vector<vector<int>> adj_list(n);
+//         for(auto con : connections) {
+//             adj_list[con[0]].push_back(con[1]);
+//             adj_list[con[1]].push_back(con[0]);
+//         }
 
-        vector<vector<int>> bridges;
-        vector<bool> visited(n, false); //initialized 0
-        // for (int i = 0; i < n; ++i) {
-        //     cout << visited[i] << endl;
-        // }
-        vector<int> jump_map(n), idx_map(n); //initialized 0
+//         vector<vector<int>> bridges;
+//         vector<bool> visited(n, false); //initialized 0
+//         // for (int i = 0; i < n; ++i) {
+//         //     cout << visited[i] << endl;
+//         // }
+//         vector<int> jump_map(n), idx_map(n); //initialized 0
 
-        dfs(0, -1, 0, adj_list, bridges,idx_map, jump_map, visited);
+//         dfs(0, -1, 0, adj_list, bridges,idx_map, jump_map, visited);
 
-        return bridges;
-    }
-};
+//         return bridges;
+//     }
+// };
 
 
 /* 这道题关键点在于
@@ -64,3 +64,92 @@ public:
                 也可以替换为：
                 jump_map[current_node] = min(jump_map[current_node], jump_map[child]);
 */
+
+
+/*
+    第二次做这道题目
+        General Idea:
+        1 dfs + depth_layer(Note not time_stamp) eg:
+            1 -- 2 -- 3
+            |_________|
+            |
+            |
+            4
+        从node 1开始访问，
+        level 0: 1
+        level 1: 4 , 2 // Note that node 4 is in level 1 not in level 4, so here is depth
+        level 2: 3
+
+
+
+        2 dfs not revisiting parent!
+
+    
+    How to find circle?
+        int dfs(parent, current):
+            for child in current children:
+                if child == parent:
+                    continue;
+                else if (not visited child):
+                    int child_return = dfs(current, child);
+
+
+    !!! Note
+    vector<vector<...>> new_list(n)
+                    
+
+*/
+
+class Solution {
+public:
+    vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connections) {
+        // convert into adj list
+        vector<vector<int>> adj_list(n);
+        // cout << "to here " << endl;
+        for (auto con:connections) {
+            adj_list[con[0]].push_back(con[1]);
+            adj_list[con[1]].push_back(con[0]);
+        }
+
+        
+        // int depth_map[n] = {-1};
+        // cout << "to here " << endl;
+        vector<int> depth_map(n, -1); //initialized 0
+        // cout << "to here " << endl;
+
+        vector<vector<int>> ret_link;
+
+        dfs(-1, 0, 0, depth_map, adj_list, ret_link);
+        return ret_link;
+        
+        
+    }
+
+
+    /*
+    dfs which not going to visit the parent node and maitain a depth_map, also keep recording the return vector
+    */
+    int dfs(int parent, int current, int level, vector<int> &depth_map, vector<vector<int>>& adj_list, vector<vector<int>>& ret_link) {
+        depth_map[current] = level;
+        for (auto child:adj_list[current]) {
+            if (child == parent) {
+                continue;
+            }
+            else if (depth_map[child] == -1) {
+                depth_map[child] = dfs(current, child, level+1, depth_map, adj_list, ret_link);
+                depth_map[current] = min(depth_map[current], depth_map[child]);
+            }
+            else {
+                depth_map[current] = min(depth_map[current], depth_map[child]);
+            }
+        }
+
+        if (depth_map[current] == level && parent != -1) {
+            cout << parent << " " << current << endl;
+            ret_link.push_back({parent, current});
+        }
+        
+        return depth_map[current];
+        
+    }
+};
--- number_of_islands.cpp
+++ number_of_islands.cpp
@@ -1,127 +1,180 @@
+// class Solution {
+// public:
+//     int numIslands(vector<vector<char>>& grid) {
+//         if (grid.empty()) {
+//             return 0;
+//         }
+//         int count = 0;
+
+//         int row = grid.size();
+//         int col = grid[0].size();
+//         for (int i = 0; i < row; ++i) {
+//             for (int j = 0; j < col; ++j) {
+//                 count += grid[i][j]- '0';
+//                 dfs(grid, i, j, row, col);
+//             }
+//         }
+//         return count;
+
+
+//     }
+// private:
+//     void dfs(vector<vector<char>>&grid, int x, int y, int m, int n) {
+//         if (x < 0 || y < 0 || x > m || y > n || grid[x][y] == '0') return;
+//         grid[x][y] = '0';
+//         dfs(grid, x-1, y, m, n);
+//         dfs(grid, x, y-1, m, n);
+//         dfs(grid, x+1, y, m, n);
+//         dfs(grid, x, y+1, m, n);
+//     }
+// };
+
+
+
+// class Solution {
+// public:
+    
+//     // struct to hold info about a cell
+//     struct cell {
+//         int row;
+//         int column;
+//     };
+    
+//     // this function connects all the possible lands together to form an island
+//     void connectIsland(int i, int j, vector<vector<char>>& grid, vector<vector<char>>& claimed)
+//     {
+//         // a queue of lands that need to be visited
+//         // and marked as claimed since it belongs to the current island
+//         queue<cell> q;
+//         cell c;
+//         c.row = i;
+//         c.column = j;
+//         claimed[c.row][c.column] = 'c'; // mark this land as claimed 'c'
+        
+//         q.push(c);
+        
+//         // keep claiming lands for the current island by claiming
+//         // those neighboring lands and explore their neigbors too
+//         while (q.empty() == false)
+//         {
+//             c = q.front();
+//             q.pop();
+//             i = c.row;
+//             j = c.column;
+            
+//             // explore all the 4 neighbors of the current land to see
+//             // if they can be claimed
+//             if (i-1 >= 0 && grid[i-1][j] == '1' && 
+//                 claimed[i-1][j] != 'c')   // only claim this neighboring land if it wasn't claimed already
+//             {
+//                 c.row = i-1;
+//                 c.column = j;
+//                 q.push(c);
+//                 claimed[c.row][c.column] = 'c'; // mark this land as claimed 'c'
+//             }
+            
+//             if (i+1 < grid.size() && grid[i+1][j] == '1' && claimed[i+1][j] != 'c')
+//             {
+//                 c.row = i+1;
+//                 c.column = j;
+//                 q.push(c);
+//                 claimed[c.row][c.column] = 'c'; // mark this land as claimed 'c'
+//             }
+            
+//             if (j-1 >= 0 && grid[i][j-1] == '1' && claimed[i][j-1] != 'c')
+//             {
+//                 c.row = i;
+//                 c.column = j-1;
+//                 q.push(c);
+//                 claimed[c.row][c.column] = 'c'; // mark this land as claimed 'c'
+//             }
+            
+//             if (j+1 < grid[0].size() && grid[i][j+1] == '1' && claimed[i][j+1] != 'c')
+//             {
+//                 c.row = i;
+//                 c.column = j+1;
+//                 q.push(c);
+//                 claimed[c.row][c.column] = 'c'; // mark this land as claimed 'c'
+//             }
+//         }
+//     }
+        
+//     int numIslands(vector<vector<char>>& grid) {
+//         // if the grid is empty, return 0
+//         if (grid.size() == 0 || grid[0].size() == 0)
+//             return 0;
+        
+//         int numOfIslands = 0;
+//         vector<vector<char>> claimed = grid;
+            
+//         // loop through each cell in the grid to find all the islands
+//         for (int i = 0; i < grid.size(); i++)
+//         {
+//             for (int j = 0; j < grid[0].size(); j++)
+//             {
+//                 // if we have discovered a new island, call connectIsland
+//                 // to connect all its lands together
+//                 if (claimed[i][j] != 'c' && grid[i][j] == '1')
+//                 {
+//                     numOfIslands++;
+//                     connectIsland(i,j, grid, claimed);
+//                 }
+//             }
+//         }
+        
+//         return numOfIslands;
+//     }
+// };
+
+
+
+/*
+    Another round
+*/
 class Solution {
 public:
     int numIslands(vector<vector<char>>& grid) {
+
         if (grid.empty()) {
             return 0;
         }
-        int count = 0;
-
-        int row = grid.size();
-        int col = grid[0].size();
-        for (int i = 0; i < row; ++i) {
-            for (int j = 0; j < col; ++j) {
-                count += grid[i][j]- '0';
-                dfs(grid, i, j, row, col);
-            }
-        }
-        return count;
-
-
-    }
-private:
-    void dfs(vector<vector<char>>&grid, int x, int y, int m, int n) {
-        if (x < 0 || y < 0 || x > m || y > n || grid[x][y] == '0') return;
-        grid[x][y] = '0';
-        dfs(grid, x-1, y, m, n);
-        dfs(grid, x, y-1, m, n);
-        dfs(grid, x+1, y, m, n);
-        dfs(grid, x, y+1, m, n);
-    }
-};
-
-
-
-class Solution {
-public:
-    
-    // struct to hold info about a cell
-    struct cell {
-        int row;
-        int column;
-    };
-    
-    // this function connects all the possible lands together to form an island
-    void connectIsland(int i, int j, vector<vector<char>>& grid, vector<vector<char>>& claimed)
-    {
-        // a queue of lands that need to be visited
-        // and marked as claimed since it belongs to the current island
-        queue<cell> q;
-        cell c;
-        c.row = i;
-        c.column = j;
-        claimed[c.row][c.column] = 'c'; // mark this land as claimed 'c'
         
-        q.push(c);
-        
-        // keep claiming lands for the current island by claiming
-        // those neighboring lands and explore their neigbors too
-        while (q.empty() == false)
-        {
-            c = q.front();
-            q.pop();
-            i = c.row;
-            j = c.column;
-            
-            // explore all the 4 neighbors of the current land to see
-            // if they can be claimed
-            if (i-1 >= 0 && grid[i-1][j] == '1' && 
-                claimed[i-1][j] != 'c')   // only claim this neighboring land if it wasn't claimed already
-            {
-                c.row = i-1;
-                c.column = j;
-                q.push(c);
-                claimed[c.row][c.column] = 'c'; // mark this land as claimed 'c'
-            }
-            
-            if (i+1 < grid.size() && grid[i+1][j] == '1' && claimed[i+1][j] != 'c')
-            {
-                c.row = i+1;
-                c.column = j;
-                q.push(c);
-                claimed[c.row][c.column] = 'c'; // mark this land as claimed 'c'
-            }
-            
-            if (j-1 >= 0 && grid[i][j-1] == '1' && claimed[i][j-1] != 'c')
-            {
-                c.row = i;
-                c.column = j-1;
-                q.push(c);
-                claimed[c.row][c.column] = 'c'; // mark this land as claimed 'c'
-            }
-            
-            if (j+1 < grid[0].size() && grid[i][j+1] == '1' && claimed[i][j+1] != 'c')
-            {
-                c.row = i;
-                c.column = j+1;
-                q.push(c);
-                claimed[c.row][c.column] = 'c'; // mark this land as claimed 'c'
-            }
-        }
-    }
-        
-    int numIslands(vector<vector<char>>& grid) {
-        // if the grid is empty, return 0
-        if (grid.size() == 0 || grid[0].size() == 0)
-            return 0;
-        
-        int numOfIslands = 0;
-        vector<vector<char>> claimed = grid;
-            
-        // loop through each cell in the grid to find all the islands
-        for (int i = 0; i < grid.size(); i++)
-        {
-            for (int j = 0; j < grid[0].size(); j++)
-            {
-                // if we have discovered a new island, call connectIsland
-                // to connect all its lands together
-                if (claimed[i][j] != 'c' && grid[i][j] == '1')
-                {
-                    numOfIslands++;
-                    connectIsland(i,j, grid, claimed);
+        // cout << grid[0][0] << endl;
+        int row_len = grid.size();
+        int col_len = grid[0].size();
+
+        int cnt = 0;
+        for (int row_i = 0; row_i < row_len; row_i++) {
+            for (int col_j = 0; col_j < col_len; col_j++) {
+                if (grid[row_i][col_j] == '1') {
+                    // cout << "ADD " << row_i << " " << col_j << endl;
+                    dfs(grid, row_i, col_j);
+                    cnt++;
                 }
+                
             }
+            
+        }
+        return cnt;
+        
+    }
+
+    /*
+        only when row(i), col(j) grid[i][j] == 1时候才运行dfs, 直接在grid上操作
+    */
+    void dfs(vector<vector<char>>& grid, int i, int j) {
+        
+        if (i < 0 || i >= grid.size() || j < 0 || j >= grid[0].size() || grid[i][j] == '0')  {
+            return;
         }
         
-        return numOfIslands;
+        if (grid[i][j] == '1') {
+            // cout << i << " " << j << " " << grid[i][j] << endl;
+            grid[i][j] = '0';
+            dfs(grid, i-1, j);
+            dfs(grid, i+1, j);
+            dfs(grid, i, j-1);
+            dfs(grid, i, j+1);
+        }
     }
 };

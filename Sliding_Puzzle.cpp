dbd3e2ef4e2934f417c61e6cf7e21a12f3ac06fa
韩宇澎 <yupeng.han@deptrum.com>
2021-01-01 21:54:50 -0500

    add 1425_0

---
+++ Sliding_Puzzle.cpp
@@ -0,0 +1,17 @@
+class Solution {
+    vector<bool> expand_map;
+    vector<vector<int>> move;
+    int ret = 0;
+    vector<int> gt1(1,2,3);
+    vector<int> gt2(4,5,0);
+public:
+    int slidingPuzzle(vector<vector<int>>& board) {    
+        vector<>
+        expand_map[]
+        if (board[0] == gt1 && board[1] == gt2) {
+            return ret;
+        }
+        else 
+    }
+    
+};
